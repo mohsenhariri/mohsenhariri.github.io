14a028c5eb05225db7ee08fc0aa9d15888574d80
Mohsen Hariri <mhariri68@gmail.com>
2024-11-26 16:10:34 -0500

    post interface

---
+++ src/types/post.ts
@@ -0,0 +1,18 @@
+type Author = {
+  name: string;
+  // picture: string;
+};
+
+export type Post = {
+  slug: string;
+  title: string;
+  date: string;
+  coverImage: string;
+  author: Author;
+  excerpt: string;
+  ogImage: {
+    url: string;
+  };
+  contentHtml: string;
+  preview?: boolean;
+};
