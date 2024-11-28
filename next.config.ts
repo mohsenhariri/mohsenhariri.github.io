aad96b77362afd31f8c135335ebce0658805db62
Mohsen Hariri <mhariri68@gmail.com>
2024-11-27 22:11:55 -0500

    redirect home to posts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/posts",
+        permanent: true, // Indicates a permanent redirect with a 308 status code
+      },
+    ];
+  },
 };
 
 export default nextConfig;
