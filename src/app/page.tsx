1df8e3175e464352241f3999e1b187ccaaf8c062
Mohsen Hariri <mhariri68@gmail.com>
2024-11-27 22:15:44 -0500

    blog list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+
+
 export default function Home() {
   return (
     <main className="flex">
       <div>
-        <h1 className="text-4xl font-bold">Hello World</h1>
-        <p className="text-lg"> LoREM IPsUm</p>
+       
+       
+
       </div>
     </main>
   );
